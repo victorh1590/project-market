1d779265bb2c7388bef227e2141f8c85816d237d
Victor Hugo <victorfaria1590@gmail.com>
2024-06-06 21:53:48 -0300

    update forms

--- frontend/src/signup-form/signup-form.component.ts
+++ frontend/src/signup-form/signup-form.component.ts
@@ -24,69 +24,69 @@ export class SignupFormComponent {
 
   private formBuilderService = inject(FormBuilder)
 
-  signupFormGroup : FormGroup = this.formBuilderService.group({
-    name : ['', 
-      [ 
-        Validators.required, 
-        Validators.minLength(3), 
-        Validators.maxLength(50)
-      ],
-      'submit'
-    ],
-    email : ['', 
-      [
-        Validators.required, 
-        Validators.email
-      ],
-      'submit'
-    ],
-    sector : ['', // TODO: Check how to declare standard value for this control in Angular. 
-      [
-        Validators.required
-      ],
-      'submit'
-    ],
-    password : ['',
-      [
-        Validators.minLength(8), 
-        Validators.maxLength(30),
-      ],
-      'submit'
-    ]
-  })
+  // signupFormGroup : FormGroup = this.formBuilderService.group({
+  //   name : ['', 
+  //     [ 
+  //       Validators.required, 
+  //       Validators.minLength(3), 
+  //       Validators.maxLength(50)
+  //     ], [],
+  //     'submit'
+  //   ],
+  //   email : ['', 
+  //     [
+  //       Validators.required, 
+  //       Validators.email
+  //     ], [],
+  //     'submit'
+  //   ],
+  //   sector : ['', // TODO: Check how to declare standard value for this control in Angular. 
+  //     [
+  //       Validators.required
+  //     ], [],
+  //     'submit'
+  //   ],
+  //   password : ['',
+  //     [
+  //       Validators.minLength(8), 
+  //       Validators.maxLength(30),
+  //     ], [],
+  //     'submit'
+  //   ]
+  // })
 
-  // signupFormGroup : FormGroup = new FormGroup(
-  //   {
-  //     name : new FormControl<string | null>('', {  
-  //       validators: [
-  //         Validators.required, 
-  //         Validators.minLength(3), 
-  //         Validators.maxLength(50)
-  //       ],
-  //       updateOn: 'submit'
-  //     }),
-  //     email : new FormControl<string | null>('', {  
-  //       validators: [
-  //         Validators.required, 
-  //         Validators.email
-  //       ],
-  //       updateOn: 'submit'
-  //     }),
-  //     sector : new FormControl<string | null>('', {  
-  //       validators: [
-  //         Validators.required
-  //       ],
-  //       updateOn: 'submit'
-  //     }),
-  //     password : new FormControl<string | null>('', {  
-  //       validators: [
-  //         Validators.minLength(8), 
-  //         Validators.maxLength(30),
-  //       ],
-  //       updateOn: 'submit'
-  //     })
-  //   }
-  // )
+  signupFormGroup : FormGroup = new FormGroup(
+    {
+      name : new FormControl<string | null>('', {  
+        validators: [
+          Validators.required, 
+          Validators.minLength(3), 
+          Validators.maxLength(50)
+        ],
+        updateOn: 'submit'
+      }),
+      email : new FormControl<string | null>('', {  
+        validators: [
+          Validators.required, 
+          Validators.email
+        ],
+        updateOn: 'submit'
+      }),
+      sector : new FormControl<string | null>('', {  
+        validators: [
+          Validators.required
+        ],
+        updateOn: 'submit'
+      }),
+      password : new FormControl<string | null>('', {  
+        validators: [
+          Validators.minLength(8), 
+          Validators.maxLength(30),
+        ],
+        updateOn: 'submit'
+      })
+    }
+  )
 
   nameIsValid : boolean = false
   emailIsValid : boolean = false
@@ -123,14 +123,13 @@ export class SignupFormComponent {
   //   })
   // }
 
-  getErrorMessagesForControl(controlName : FormControl) : string[]
+  getErrorMessagesForControl(controlName : String, controlErrors : ValidationErrors | null | undefined) : string[]
   {
-
     // let nameErrors : ValidationErrors | null | undefined = this.signupFormGroup.controls.controlName[`${controlName}`]?.errors;
-    return controlName.errors != null ? this.formatValidationMessage(controlName, controlName.errors) : []
+    return (controlErrors != null && controlErrors != undefined) ? this.formatValidationMessage(controlName, controlErrors) : []
   }
 
-  formatValidationMessage(controlName : FormControl, errors : ValidationErrors) : string[]
+  formatValidationMessage(controlName : String, errors : ValidationErrors) : string[]
   {
     let messages: string[] = []
     for(let error in errors)
@@ -138,13 +137,13 @@ export class SignupFormComponent {
       switch(error)
       {
         case "required":
-          messages.push(`The field ${controlName} is required.`);
+          messages.push(`The field "${controlName}" is required.`);
           break;
         case "minlength":
-          messages.push(`The minimum length of the field ${controlName} is ${errors['minlength'].requiredLength}.`)
+          messages.push(`The minimum length of the field "${controlName}" is ${errors['minlength'].requiredLength}.`)
           break;
         case "maxlength":
-          messages.push(`The maximum length of the field ${controlName} is ${errors['maxlength'].requiredLength}.`)
+          messages.push(`The maximum length of the field "${controlName}" is ${errors['maxlength'].requiredLength}.`)
           break;
         case "email":
           messages.push(`The email entered is invalid.`)
