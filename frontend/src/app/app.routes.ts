8b1294c0bc2c07654e190d42cea77a47e22d7b7f
Victor Hugo <victorfaria1590@gmail.com>
2024-06-30 15:05:06 -0300

    Revert "feat: project now uses react instead of angular"
    This reverts commit e6fe6fa2048f65e07dd1275926f24cc1224f0bfe.

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { SignupFormComponent } from '../signup-form/signup-form.component';
+
+export const routes: Routes = [
+    { path: '', component: SignupFormComponent, title: 'Signup Form'}
+];
