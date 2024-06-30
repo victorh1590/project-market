15987ff25951357bb3c187f6a10f71c577a0fca0
Victor Hugo <victorfaria1590@gmail.com>
2024-06-30 15:10:19 -0300

    Delete all files in master branch

--- frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { SignupFormComponent } from '../signup-form/signup-form.component';
-
-export const routes: Routes = [
-    { path: '', component: SignupFormComponent, title: 'Signup Form'}
-];
