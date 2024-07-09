a369f4ae25983d2e70896deacb539d7b8c85ef39
Victor Hugo <victorfaria1590@gmail.com>
2024-07-08 21:49:15 -0300

    feat: unit of work classes and ticket model classes

---
+++ backend/SupportCentral.Server/Infra/Db/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace SupportCentral.Server;
+
+public interface IUnitOfWork
+{
+    public IDbConnection Connection { get; }
+    public void Begin();
+    public void Commit();
+    public void Rollback();
+    public void Dispose();
+
+}
---
+++ backend/SupportCentral.Server/Infra/Db/UnitOfWork.cs
@@ -0,0 +1,42 @@
+using Npgsql;
+using System.Data;
+
+namespace SupportCentral.Server.Infra.Db;
+
+public sealed class UnitOfWork : IUnitOfWork, IDisposable
+{
+    public IDbConnection Connection { get; }
+    private IDbTransaction? _transaction;
+
+    public UnitOfWork(IConfiguration configuration) {
+        Connection = new NpgsqlConnection(configuration.GetConnectionString("postgresql"));
+        Connection.Open();
+    }
+    public void Begin() {
+        _transaction = Connection.BeginTransaction();
+    }
+
+    public void Commit()
+    {
+        try
+        {
+            _transaction?.Commit();
+        }
+        catch
+        {
+            _transaction?.Rollback();
+            throw;
+        }
+    }
+
+    public void Rollback()
+    {
+        _transaction?.Rollback();
+    }
+
+    public void Dispose()
+    {
+        _transaction?.Dispose();
+        Connection.Dispose();
+    }
+}
