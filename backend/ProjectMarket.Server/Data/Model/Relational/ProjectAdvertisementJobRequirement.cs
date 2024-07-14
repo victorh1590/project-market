aa5ff3e21a52985b5ad992fd33b14a45e3479094
Victor Hugo <victorfaria1590@gmail.com>
2024-07-13 22:19:47 -0300

    same as last commit

--- backend/ProjectMarket.Server/Data/Model/Relational/ProjectAdvertisementJobRequirement.cs
+++ backend/ProjectMarket.Server/Data/Model/Relational/ProjectAdvertisementJobRequirement.cs
@@ -5,6 +5,4 @@ namespace ProjectMarket.Server.Data.Model.VO;
 public class ProjectAdvertisementJobRequirementVO {
     required public int ProjectAdvertisement { get; set; }
     required public int JobRequirement { get; set; }
-    //
-    public List<ProjectAdvertisement>? Advertisements { get; set; }
 }
